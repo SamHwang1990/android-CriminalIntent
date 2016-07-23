04ac1e08b6337184e3036f5186abcd371c641daf
SamHwang1990 <samhwang1990@gmail.com>
2016-07-23 08:14:33 +0800

    init commit

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -0,0 +1,54 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+/**
+ * Created by sam on 16/7/23.
+ */
+public class CrimeFragment extends Fragment {
+
+    private Crime mCrime;
+    private EditText mTextField;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        // 该方法仅用于配置fragment
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_crime, container, false);
+
+        mTextField = (EditText) v.findViewById(R.id.crime_title);
+        mTextField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                mCrime.setTitle(charSequence.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        return v;
+    }
+
+
+
+}
