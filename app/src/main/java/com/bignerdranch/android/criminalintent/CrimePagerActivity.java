7dafcd632bd6b745e94ff566e86e5c87f7a47cf8
SamHwang1990 <samhwang1990@gmail.com>
2016-07-24 20:43:53 +0800

    complete the 11th chapter practice: 1. add ViewPager and FragmentStatePagerAdapter;

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
@@ -0,0 +1,62 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Created by sam on 16/7/24.
+ */
+public class CrimePagerActivity extends FragmentActivity {
+
+    private static final String EXTRA_CRIME_ID = "com.bignerdranch.android.criminalintent.crime_id";
+
+    private ViewPager mViewPager;
+    private List<Crime> mCrimes;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime_pager);
+
+        mViewPager = (ViewPager) findViewById(R.id.activity_crime_pager_view_pager);
+        mCrimes = CrimeLab.get().getCrimes();
+
+        FragmentManager fm = getSupportFragmentManager();
+        mViewPager.setAdapter(new FragmentStatePagerAdapter(fm) {
+            @Override
+            public Fragment getItem(int position) {
+                return CrimeFragment.newInstance(mCrimes.get(position).getID());
+            }
+
+            @Override
+            public int getCount() {
+                return mCrimes.size();
+            }
+        });
+
+        UUID entryCrimeId = (UUID) getIntent().getSerializableExtra(EXTRA_CRIME_ID);
+        for (int i = 0; i < mCrimes.size(); ++i) {
+            if (mCrimes.get(i).getID().equals(entryCrimeId)) {
+                mViewPager.setCurrentItem(i);
+                break;
+            }
+        }
+
+    }
+
+    public static Intent newIntent(Context context, UUID crimeId) {
+        Intent intent = new Intent(context, CrimePagerActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID, crimeId);
+        return intent;
+    }
+
+}
